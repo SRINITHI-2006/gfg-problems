8ecca1d302e2b1a085bf147e160fe62fc14f4e72
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-03-10 22:52:26 +0530

    Create ReplaceGreatestElementOnRight.java

---
+++ ReplaceGreatestElementOnRight.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+      int n = arr.length;
+        int[] res = new int[n];
+
+        res[n-1] = -1;
+        int max = arr[n-1];
+
+        for(int i = n-2; i >= 0; i--) {
+            res[i] = max;
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return res;
+    }
+}
